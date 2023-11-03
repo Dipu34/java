5c4316d24d5208b96d987a3816bf3e81b5e2aed6
Dipendra Sah <131299802+Dipu34@users.noreply.github.com>
2023-11-03 20:05:43 +0530

    java

---
+++ q9.java
@@ -0,0 +1,33 @@
+package ass3A;
+import java.util.*;
+public class q9 {
+	 static void printUnion(int[] a, int n, int[] b, int m)
+	    {
+	        Map<Integer, Integer> mp
+	            = new HashMap<Integer, Integer>();
+	 
+	        for (int i = 0; i < n; i++) {
+	            mp.put(a[i], i);
+	        }
+	        for (int i = 0; i < m; i++) {
+	            mp.put(b[i], i);
+	        }
+	 
+	        System.out.println(
+	            "The union set of both arrays is :");
+	        for (Map.Entry mapElement : mp.entrySet()) {
+	            System.out.print(mapElement.getKey() + " ");
+	 
+	        }
+	    }
+	 
+	    // Driver Code
+	    public static void main(String[] args)
+	    {
+	        int a[] = { 1, 2, 5, 6, 2, 3, 5 };
+	        int b[] = { 2, 4, 5, 6, 8, 9, 4, 6, 5 };
+	 
+	        printUnion(a, 7, b, 9);
+	    }
+	}
+
